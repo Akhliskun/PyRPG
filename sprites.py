1a1a5d1298459cee0c9bd9d6519743c135d5c0cd
Danut Labici <labicidanut@gmail.com>
2018-04-19 01:59:19 +0300

    * Add basic player states:   - Standing / Idle   - Walking Left/Right   - Jumping * Add animate engine. Really rudimentary   - One good thing about it is that I don't need to care how many frames my animation has. It will always load all and wrap around the list. * Add Walking/Standing animation in game :)   - Bunny is alive!

--- sprites.py
+++ sprites.py
@@ -20,13 +20,34 @@ class Player(pg.sprite.Sprite):
     def __init__(self, game):
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        self.image = self.game.spritesheet.get_image(614, 1063, 120, 191)
-        self.image.set_colorkey(BLACK)
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.standing_frames[0]
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+
+    def load_images(self):
+        self.standing_frames = [self.game.spritesheet.get_image(614, 1063, 120, 191),
+                                self.game.spritesheet.get_image(690, 406, 120, 201)]
+        for frame in self.standing_frames:
+            frame.set_colorkey(BLACK)  # Fix transparency
+
+        self.walk_frames_r = [self.game.spritesheet.get_image(678, 860, 120, 201),
+                              self.game.spritesheet.get_image(692, 1458, 120, 207)]
+        for frame in self.standing_frames:
+            frame.set_colorkey(BLACK)  # Fix transparency
+
+        self.walk_frames_l = []  # Flip walking_frames_r
+        for frame in self.walk_frames_r:
+            self.walk_frames_l.append(pg.transform.flip(frame, True, False))
+        self.jump_frame = self.game.spritesheet.get_image(382, 763, 150, 181)
+        self.jump_frame.set_colorkey(BLACK)  # Fix transparency
 
     def jump(self):
         # Jump only if stating on a platform
@@ -37,6 +58,7 @@ class Player(pg.sprite.Sprite):
             self.vel.y = -PLAYER_JUMPPOWER
 
     def update(self):
+        self.animate()
         self.acc = vec(0, PLAYER_GRAVITY)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
@@ -59,6 +81,16 @@ class Player(pg.sprite.Sprite):
 
         self.rect.midbottom = self.pos
 
+    def animate(self):
+        now = pg.time.get_ticks()
+        if not self.jumping and not self.walking:
+            if now - self.last_update > 350:  # TODO: Move this to settings.py
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                bottom = self.rect.bottom
+                self.image = self.standing_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
 
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
