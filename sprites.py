9b28999e50c686db6d0a7965479708c40534d1f5
Danut Ioan Labici <danut.labici@softvision.ro>
2018-04-20 15:58:29 +0300

    Redone player position and velocity into Vector2. Added graphics to the player character

--- sprites.py
+++ sprites.py
@@ -2,67 +2,63 @@ import pygame as pg
 
 from settings import *
 
+vec = pg.math.Vector2
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(YELLOW)
+        self.image = game.player_img
         self.rect = self.image.get_rect()
-        self.vx = 0
-        self.vy = 0
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
+        self.vel = vec(x=0, y=0)
+        self.pos = vec(x=x, y=y) * TILESIZE
 
     def get_keys(self):
-        self.vx = 0
-        self.vy = 0
+        self.vel = vec(x=0, y=0)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vel.x = -PLAYER_SPEED
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vx = PLAYER_SPEED
+            self.vel.x = PLAYER_SPEED
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vy = -PLAYER_SPEED
+            self.vel.y = -PLAYER_SPEED
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vy = PLAYER_SPEED
+            self.vel.y = PLAYER_SPEED
 
         # Detect diagonal movement and apply pythagoras theorem
         # This will keep the speed of diagonal movements the same
         # with horizontal / vertical movement
-        if (self.vx != 0) and (self.vy != 0):
-            self.vx *= 0.7071
-            self.vy *= 0.7071
+        if (self.vel.x != 0) and (self.vel.y != 0):
+            self.vel *= 0.7071
 
     def collide_with_walls(self, dir):
         if dir == 'x':
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vx > 0:
-                    self.x = hits[0].rect.left - self.rect.width
-                if self.vx < 0:
-                    self.x = hits[0].rect.right
-                self.vx = 0
-                self.rect.x = self.x
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
         if dir == 'y':
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.width
-                if self.vy < 0:
-                    self.y = hits[0].rect.bottom
-                self.vy = 0
-                self.rect.y = self.y
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.width
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
 
     def update(self):
         self.get_keys()
-        self.x += self.vx * self.game.dt
-        self.y += self.vy * self.game.dt
-        self.rect.x = self.x
+        self.pos += self.vel * self.game.dt
+        self.rect.x = self.pos.x
         self.collide_with_walls('x')
-        self.rect.y = self.y
+        self.rect.y = self.pos.y
         self.collide_with_walls('y')
 
 
