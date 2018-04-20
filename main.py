68f06eaeb1156b9e24c09ea39a036bbb91ed74ef
Danut Ioan Labici <danut.labici@softvision.ro>
2018-04-20 11:44:28 +0300

    Added general map loading, player movement and wall collision

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# KidsCanCode - Game Development with Pygame video series
-# Tile-based game - Part 1
-# Project setup
-# Video link: https://youtu.be/3UxnelT9aCo
 import pygame as pg
 import sys
+from os import path
 from settings import *
 from sprites import *
 
@@ -13,19 +10,27 @@ class Game:
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
+        pg.key.set_repeat(100, 100)
         self.load_data()
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, "map.txt"), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
+
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.player = Player(self, 10, 10)
-        for x in range(10, 20):
-            Wall(self, x, 5)
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
 
     def run(self):
         # game loop - set self.playing = False to end the game
