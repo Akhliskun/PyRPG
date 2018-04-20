79e231eb1ca8e44bb789c8c3931d0b045c760180
Danut Ioan Labici <danut.labici@softvision.ro>
2018-04-20 17:27:35 +0300

    Added basic enemy feature.

--- settings.py
+++ settings.py
@@ -9,21 +9,25 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BROWN = (106, 55, 5)
+
 # game settings
-WIDTH = 1024  # 16 * 64 or 32 * 32 or 64 * 16
-HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+WIDTH = 1024
+HEIGHT = 768
 FPS = 60
 TITLE = "PyRPG"
 BGCOLOR = BROWN
 
-TILESIZE = 32
+TILESIZE = 64
 GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 
-WALL_IMG = 'walltile.png'
+WALL_IMG = 'tileGreen_39.png'
 
 # Player settings
-PLAYER_SPEED = 200
-PLAYER_ROTATION_SPEED = 250
+PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 250
 PLAYER_IMG = 'manBlue_gun.png'
 PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+
+# Mob settings
+MOB_IMG = 'zombie1_hold.png'
--- sprites.py
+++ sprites.py
@@ -1,6 +1,6 @@
 import pygame as pg
-from tilemap import collide_hit_rect
 from settings import *
+from tilemap import collide_hit_rect
 
 vec = pg.math.Vector2
 
@@ -16,32 +16,20 @@ class Player(pg.sprite.Sprite):
         self.hit_rect.center = self.rect.center
         self.vel = vec(x=0, y=0)
         self.pos = vec(x=x, y=y) * TILESIZE
-        self.rotation = 0
+        self.rot = 0
 
     def get_keys(self):
-        self.rotation_speed = 0
+        self.rot_speed = 0
         self.vel = vec(x=0, y=0)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.rotation_speed = PLAYER_ROTATION_SPEED
+            self.rot_speed = PLAYER_ROT_SPEED
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.rotation_speed = -PLAYER_ROTATION_SPEED
+            self.rot_speed = -PLAYER_ROT_SPEED
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vel = vec(x=PLAYER_SPEED, y=0).rotate(-self.rotation)
+            self.vel = vec(x=PLAYER_SPEED, y=0).rotate(-self.rot)
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vel = vec(x=-PLAYER_SPEED / 2, y=0).rotate(self.rotation)
-        if keys[pg.K_q]:
-            self.vel.x = -PLAYER_SPEED
-        if keys[pg.K_e]:
-            self.vel.x = PLAYER_SPEED
-
-        # Detect diagonal movement and apply pythagoras theorem
-        # This will keep the speed of diagonal movements the same
-        # with horizontal / vertical movement
-        # if (self.vel.x != 0) and (self.vel.y != 0):
-        #     self.vel *= 0.7071
-        # DON'T NEED THIS ANYMORE AND WE INTRODUCED FREE MOVEMENT
-        # GONNA KEEP THIS HERE FOR LATER USAGE?
+            self.vel = vec(x=-PLAYER_SPEED / 2, y=0).rotate(-self.rot)
 
     def collide_with_walls(self, dir):
         if dir == 'x':
@@ -57,24 +45,42 @@ class Player(pg.sprite.Sprite):
             hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
             if hits:
                 if self.vel.y > 0:
-                    self.pos.y = hits[0].rect.top - self.hit_rect.width / 2
+                    self.pos.y = hits[0].rect.top - self.hit_rect.height / 2
                 if self.vel.y < 0:
-                    self.pos.y = hits[0].rect.bottom + self.hit_rect.width / 2
+                    self.pos.y = hits[0].rect.bottom + self.hit_rect.height / 2
                 self.vel.y = 0
                 self.hit_rect.centery = self.pos.y
 
     def update(self):
         self.get_keys()
-        self.rotation = (self.rotation + self.rotation_speed * self.game.dt) % 360
-        self.image = pg.transform.rotate(self.game.player_img, self.rotation)
+        self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
+        self.image = pg.transform.rotate(self.game.player_img, self.rot)
         self.rect = self.image.get_rect()
-        self.hit_rect.center = self.pos
+        self.rect.center = self.pos
         self.pos += self.vel * self.game.dt
         self.hit_rect.centerx = self.pos.x
         self.collide_with_walls('x')
         self.hit_rect.centery = self.pos.y
         self.collide_with_walls('y')
         self.rect.center = self.hit_rect.center
+
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.mob_img
+        self.rect = self.image.get_rect()
+        self.pos = vec(x=x, y=y) * TILESIZE
+        self.rect.center = self.pos
+        self.rot = 0
+
+    def update(self):
+        self.rot = (self.game.player.pos - self.pos).angle_to(vec(x=1, y=0))
+        self.image = pg.transform.rotate(self.game.mob_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
 
 
 class Wall(pg.sprite.Sprite):
